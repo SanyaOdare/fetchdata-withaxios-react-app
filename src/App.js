4c44c793b3388e87e3754af2b9483f2242c2a205
SanyaOdare <sanyaodare@gmail.com>
2023-01-29 16:05:11 +0100

    save all changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [ users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users?_limit=5")
+    .then(res => {
+      console.log(res.data)
+      setUsers(res.data)
+    })
+    .catch(error => console.log(error));
+    
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetch Data With Axios</h1>
+      {users && users.map((user) => {
+          return (
+            <div className="Users-container" key={user.id}>
+              <p className="Name">{user.name}</p>
+              <p className="Phone-number">{user.phone}</p>
+              <div className="Address">
+                <p className="Email">{user.email}</p>
+                <p className="Email">{user.address.street}</p>
+                <p className="Email">{user.address.suite}</p>
+                <p className="Email">{user.address.city}</p>
+                <p className="Email">{user.address.zipcode}</p>
+                <p className="Email">{user.address.geo.lat}</p>
+              </div>
+            </div>
+        );
+      })}
     </div>
   );
 }
